2079b0d55b76e08af2cf0884f54ff8e163de84fb
mrdoggel <61916718+mrdoggel@users.noreply.github.com>
2023-03-31 18:22:46 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module cfgimport.cfgimport {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens cfgimport.pack to javafx.fxml;
-    exports cfgimport.pack;
-}
